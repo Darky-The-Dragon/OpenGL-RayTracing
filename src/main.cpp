fc8e961b00368cbac4a366c0fcd005b73cb5c85f
Darky <dim4ik12092000.dz@gmail.com>
2025-05-30 17:54:11 +0200

    OpenGL Test main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
-//
-// Created by Darky on 29/05/25.
-//
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-#include "main.h"
+int main() {
+    if (!glfwInit()) return -1;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.0f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
